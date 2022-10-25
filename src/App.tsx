6819a28159b822af83400d7971a8578df9bbeea6
SVA2021 <sofronov.vit@gmail.com>
2022-10-25 21:28:44 +0400

    initial style files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit provident vero tempora delectus accusantium, a alias laborum tempore explicabo iusto. Quae consequatur ipsa in maxime provident reiciendis eius voluptatem numquam!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas rem corporis et animi quos, eos dignissimos aliquam obcaecati at sit. Natus fugiat aliquid quaerat expedita possimus ea recusandae, cumque minima.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt veritatis vero facere voluptates, pariatur totam laborum sed reprehenderit provident. In expedita doloremque mollitia nisi unde facilis delectus voluptatibus hic voluptate?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur suscipit delectus modi eum, id veritatis molestiae placeat distinctio nisi obcaecati adipisci. Sequi non quod hic, vel enim cumque sapiente debitis?
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum provident consequuntur excepturi similique sit laborum nihil reprehenderit. Quam sequi iusto doloremque cupiditate quos minima, id, temporibus, velit consequuntur inventore ipsam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nisi voluptates voluptate soluta ea ducimus nostrum blanditiis inventore hic odit totam quo corporis, velit mollitia dignissimos eaque ex esse libero.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis provident veritatis veniam omnis necessitatibus nesciunt at quia laboriosam, nulla quis praesentium minus hic voluptatum et modi asperiores fugiat voluptatem ratione!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi eius ab, velit iusto quidem natus sed pariatur iure consectetur earum autem ea ut itaque similique ex debitis aperiam? Quod, iusto.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, qui facere optio earum, architecto beatae, unde distinctio quae magnam iusto libero harum ab sequi quam aliquam quaerat. Similique, mollitia repellat!
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus vero totam natus, sed modi asperiores facere molestiae reiciendis suscipit libero aut fugiat. In, dolore consequuntur. Et aut ad qui numquam.
     </div>
   );
 }
